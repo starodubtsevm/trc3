d8bdc2c83671078e92f8fd6c503bbc5e2dc72f66
starodubtsevm <starodubtsevm@gmail.com>
2022-02-27 22:30:15 +0500

    начата работа над gui

--- Trc_tx_mes_GUI/src/main.py
+++ Trc_tx_mes_GUI/src/main.py
@@ -9,23 +9,20 @@ def create_signals():
         print("fs = " + str(fs) + " Hz")
         print("fs2 = " + str(fs2) + " Hz")
         print("")
-
         #- Конфигурирование генераторов
         print("------------------------------------")
         print("старт расчета смеси входных сигналов")
         print("")
-
         prep = time()
         buf_mix_signals, Signals = mix_signals()
         print("время расчета - %s sec " % round((time() - prep),2))
-
         print("")
         print("сгенерированные сигналы")
         print("")
         print(*Signals, sep="\n")
         print("------------------------------------")
         print("")
-        return buf_mix_signals, Signals
+        return buf_mix_signals
 
 
 def create_receiver(buff_mix_signals):
@@ -34,7 +31,6 @@ def create_receiver(buff_mix_signals):
         print("")
         print(f"приемник {f_rx} Гц {f_mod} Гц")
         print("")
-
         rx = time()
         krl_rec1 = krl_receiver(f_rx, f_mod)
         krl_rec_out_buffers = krl_rec1.proc(buf_mix_signals)
@@ -43,20 +39,18 @@ def create_receiver(buff_mix_signals):
         print("------------------------------------")
         return krl_rec_out_buffers
 
+
 def plot_results(krl_rec_out_buffers):
         #--Построение графиков---------------
-        #plot = time.time()
         print("построение выходных графиков")
-
         to_plot(krl_rec_out_buffers, mix_signals)
         #plotSpectrum(mix_signals)
-
-        #print("время расчета - %s sec " % (t   ime.time()-plot))
         print("")
         print("------------------------------------")
         return 0
 
-buf_mix_signals, Signals = create_signals()
+
+buf_mix_signals = create_signals()
 krl_rec_out_buffers = create_receiver(buf_mix_signals)
 plot_results(krl_rec_out_buffers)
 
